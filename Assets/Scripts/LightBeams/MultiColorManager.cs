bea7345e50d742b36e14c9d37771dd8efc2a730d
JunoStrunk <junostrunk@gmail.com>
2024-01-14 04:32:45 -0500

    MultiColorManager

--- Assets/Scripts/LightBeams/MultiColorManager.cs
+++ Assets/Scripts/LightBeams/MultiColorManager.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class MultiColorManager : MonoBehaviour
 {
+    public List<I_LDest> dests;
+    int numHitDests = 0;
+
     public void destHit()
     {
+        numHitDests++;
+        if (numHitDests >= dests.Count)
+            Activate();
+    }
 
+    public virtual void Activate()
+    {
+        Debug.Log("Do something");
     }
 }
